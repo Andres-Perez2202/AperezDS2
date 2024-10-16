489e64e8fbba2a2a2a86fe1cb7f48ef3288fa68f
USER <USER@DESKTOP-D02R3BC>
2024-10-16 13:41:23 -0500

    Agregar archivos de proyecto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AperezDS2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
